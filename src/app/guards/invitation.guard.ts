7d3d574c5f295d3a34c602840f7eead7673bb716
vanessa <vanessag.core@gmail.com>
2026-07-28 14:41:05 -0600

    guard fixed

--- src/app/guards/invitation.guard.ts
+++ src/app/guards/invitation.guard.ts
@@ -1,6 +1,15 @@
 import { CanActivateFn, Router } from '@angular/router';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 export const invitationGuard: CanActivateFn = () => {
+
+  const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
+
+  if (!isPlatformBrowser(platformId)) {
+    return true;
+  }
 
   const entered = sessionStorage.getItem('invitationEntered');
 
@@ -8,8 +17,6 @@ export const invitationGuard: CanActivateFn = () => {
     sessionStorage.removeItem('invitationEntered');
     return true;
   }
-
-  const router = new Router();
 
   return router.createUrlTree(['/']);
 
